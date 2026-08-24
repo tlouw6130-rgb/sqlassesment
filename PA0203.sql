8cde3122c4d62b4a6877fb245c24af528096999f
Tshepo Louw <tlouw6130@gmail.com>
2026-08-24 08:15:06 +0200

    Add files via upload

---
+++ PA0203.sql
@@ -0,0 +1,10 @@
+/******************************************************************
+PA0203 - Eliminate Duplicates Using DISTINCT
+******************************************************************/
+USE TrainingAcademyDB; 
+SELECT DISTINCT City
+From Students;
+SELECT DISTINCT EnrollmentStatus
+From Enrollments;
+SELECT DISTINCT PaymentMethod
+From Payments;
